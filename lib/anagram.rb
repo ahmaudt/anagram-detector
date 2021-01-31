0d5f4ae616f9cdb4935f010d1a64d31245d6458a
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-30 22:16:05 -0500

    commit tree 5d514bfee912d5ae0cd3a1c455b18a5693759481

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(items)
+        items.select do |term|
+            puts term.split("")
+            term.downcase.split("").sort == @word.downcase.split("").sort
+        end
+        # items.select { |entry| puts entry }
+    end
+
+end
